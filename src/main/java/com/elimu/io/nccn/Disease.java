203e6c781e5a7b74ffbd5e3eac5a742231afc0ec
aziz <>
2018-06-01 01:55:00 +0000

    {/src/main/java/com/elimu/io/nccn/Disease.java}

--- src/main/java/com/elimu/io/nccn/Disease.java
+++ src/main/java/com/elimu/io/nccn/Disease.java
@@ -6,12 +6,35 @@ package com.elimu.io.nccn;
 
 public class Disease implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Disease() {
-    }
+	@org.kie.api.definition.type.Label(value = "type")
+	private java.lang.String type;
+	@org.kie.api.definition.type.Label(value = "status")
+	private java.lang.String status;
 
+	public Disease() {
+	}
 
+	public java.lang.String getType() {
+		return this.type;
+	}
 
+	public void setType(java.lang.String type) {
+		this.type = type;
+	}
+
+	public java.lang.String getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(java.lang.String status) {
+		this.status = status;
+	}
+
+	public Disease(java.lang.String type, java.lang.String status) {
+		this.type = type;
+		this.status = status;
+	}
 
 }
