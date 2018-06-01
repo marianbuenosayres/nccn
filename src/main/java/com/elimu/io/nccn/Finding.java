84faa2d3be86bc402c38299ae41b4ed9ff1a13b8
aziz <>
2018-06-01 01:49:52 +0000

    {/src/main/java/com/elimu/io/nccn/Finding.java}

--- src/main/java/com/elimu/io/nccn/Finding.java
+++ src/main/java/com/elimu/io/nccn/Finding.java
@@ -6,12 +6,35 @@ package com.elimu.io.nccn;
 
 public class Finding implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Finding() {
-    }
+	@org.kie.api.definition.type.Label(value = "name")
+	private java.lang.String name;
+	@org.kie.api.definition.type.Label(value = "value")
+	private java.lang.String value;
 
+	public Finding() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.String getValue() {
+		return this.value;
+	}
+
+	public void setValue(java.lang.String value) {
+		this.value = value;
+	}
+
+	public Finding(java.lang.String name, java.lang.String value) {
+		this.name = name;
+		this.value = value;
+	}
 
 }
